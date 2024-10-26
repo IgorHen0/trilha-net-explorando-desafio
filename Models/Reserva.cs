e9514f7053a37c76fe80260a96e09a6b7305d76c
IgorHen0 <igorhenrique0472@gmail.com>
2024-10-26 11:15:04 -0300

    construindo um sistema de hospedagem de um hotel

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,7 +17,10 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            int capacidade = Suite.Capacidade;
+            int quantidadeHospedes = hospedes.Count;
+
+            if (capacidade >= quantidadeHospedes)
             {
                 Hospedes = hospedes;
             }
@@ -25,6 +28,7 @@ namespace DesafioProjetoHospedagem.Models
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                throw new Exception("Capacidade da suíte menor que o número de hospedes recebido");
             }
         }
 
@@ -37,7 +41,8 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            int quantidadeHospedes = Hospedes.Count;
+            return quantidadeHospedes;
         }
 
         public decimal CalcularValorDiaria()
@@ -47,11 +52,13 @@ namespace DesafioProjetoHospedagem.Models
             // *IMPLEMENTE AQUI*
             decimal valor = 0;
 
+            valor = DiasReservados * Suite.ValorDiaria;
+
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor *= 0.9M;
             }
 
             return valor;
